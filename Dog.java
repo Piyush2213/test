119cd808cd0460322996e389f00bbabdc98c578b
Piyush Yadav <piyushyadav@Piyushs-MacBook-Pro.local>
2024-12-13 12:46:49 +0530

    first commit

---
+++ Dog.java
@@ -0,0 +1,12 @@
+
+public class Dog extends Animal {
+    public Dog(){
+        System.out.println("Dog contructor called");
+    }
+    public void sayHello(){
+        System.out.println("Woof!");
+    }
+    public void sayBye(){
+        System.out.println("Woof! Woof!");
+    }
+}
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test{
+    public static void main(String[] args){
+        Dog dog = new Dog();
+        dog.setAge(15);
+        dog.sayHello();
+        dog.sayBye();
+    }
+
+}
